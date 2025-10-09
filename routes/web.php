43950840d44c7ae7431263e400f0c9ee172e49f1
shubhamedtech <shubham@edtechinnovate.com>
2025-10-09 10:36:43 +0530

    web routes

--- routes/web.php
+++ routes/web.php
@@ -71,21 +71,21 @@ Route::prefix('settings')->group(function () {
 });
 
 
-Route::get('/menu', [MenuController::class, 'index'])->name('menu.index');
-Route::get('/menu/create', [MenuController::class, 'create'])->name('menu.create');
-Route::post('/menu/store', [MenuController::class, 'store'])->name('menu.store');
-Route::get('/menu/edit/{id}', [MenuController::class, 'edit'])->name('menu.edit');
-Route::post('/menu/update/{id}', [MenuController::class, 'update'])->name('menu.update');
-Route::delete('/menu/destroy/{id}', [MenuController::class, 'destroy'])->name('menu.destroy');
-Route::get('menu/status/{id}', [MenuController::class, 'status'])->name('menu.status');
+    Route::get('/menu', [MenuController::class, 'index'])->name('menu.index');
+    Route::get('/menu/create', [MenuController::class, 'create'])->name('menu.create');
+    Route::post('/menu/store', [MenuController::class, 'store'])->name('menu.store');
+    Route::get('/menu/edit/{id}', [MenuController::class, 'edit'])->name('menu.edit');
+    Route::post('/menu/update/{id}', [MenuController::class, 'update'])->name('menu.update');
+    Route::delete('/menu/destroy/{id}', [MenuController::class, 'destroy'])->name('menu.destroy');
+    Route::get('menu/status/{id}', [MenuController::class, 'status'])->name('menu.status');
 
-Route::get('/theme', [ThemeController::class, 'index'])->name('theme.index');
-Route::get('/theme/create', [ThemeController::class, 'create'])->name('theme.create');
-Route::post('/theme/store', [ThemeController::class, 'store'])->name('theme.store');
-Route::get('/theme/edit/{id}', [ThemeController::class, 'edit'])->name('theme.edit');
-Route::post('/theme/update/{id}', [ThemeController::class, 'update'])->name('theme.update');
-Route::delete('/theme/destroy/{id}', [ThemeController::class, 'destroy'])->name('theme.destroy');
-Route::get('theme/status/{id}', [ThemeController::class, 'status'])->name('theme.status');
+    Route::get('/theme', [ThemeController::class, 'index'])->name('theme.index');
+    Route::get('/theme/create', [ThemeController::class, 'create'])->name('theme.create');
+    Route::post('/theme/store', [ThemeController::class, 'store'])->name('theme.store');
+    Route::get('/theme/edit/{id}', [ThemeController::class, 'edit'])->name('theme.edit');
+    Route::post('/theme/update/{id}', [ThemeController::class, 'update'])->name('theme.update');
+    Route::delete('/theme/destroy/{id}', [ThemeController::class, 'destroy'])->name('theme.destroy');
+    Route::get('theme/status/{id}', [ThemeController::class, 'status'])->name('theme.status');
 
 
 
